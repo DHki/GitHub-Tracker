2576c0486069f39619d9516de1a614a8a9e13871
dhki <kdh101800@gmail.com>
2024-12-29 21:32:23 +0900

    feat: main app, requirements

---
+++ src/app.py
@@ -0,0 +1,74 @@
+import os
+import json
+import time
+import discord
+import asyncio
+import threading
+from datetime import datetime, timezone, timedelta
+
+from utils.github_api import *
+from utils.discord_bot import *
+
+GITHUB_API_TOKEN = os.getenv("GITHUB_API_TOKEN")
+DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+DISCORD_GUILD_ID = os.getenv("DISCORD_GUILD_ID")
+
+def check_and_send(repo, last_check_time):
+    if repo["release"]:
+        r_update = github_check_release(last_check_time, GITHUB_API_TOKEN, repo)
+        if r_update.get("update", False):
+            for d in r_update['data']:
+                asyncio.run(notify_to_channel("r", repo['repository'], d))
+
+    if repo["issue"]["check"]:
+        i_update = github_check_issue(last_check_time, GITHUB_API_TOKEN, repo)
+        if i_update.get("update", False):
+            for d in i_update['data']:
+                notify_to_channel("i", repo['repository'], d)
+
+    if repo["pull_request"]["check"]:
+        pr_update = github_check_pull_request(last_check_time, GITHUB_API_TOKEN, repo)
+        if pr_update.get("update", False):
+            for d in pr_update['data']:
+                asyncio.run(notify_to_channel("pr", repo['repository'], d))
+
+    if repo["commit"]["check"]:
+        c_update = github_check_commit(last_check_time, GITHUB_API_TOKEN, repo)
+        if c_update.get("update", False):
+            for d in c_update['data']:
+                asyncio.run(notify_to_channel("c", repo['repository'], d))
+
+def notify_to_channel(kind, channel, obj):
+    intents = discord.Intents.default()
+    bot = DiscordBot(intents=intents, guild_id=DISCORD_GUILD_ID)
+    
+    embed = get_embed(kind=kind, obj=obj)
+    bot.set_channel(channel=channel)
+    bot.set_message(embed=embed)
+
+    bot.run(token=DISCORD_BOT_TOKEN)
+        
+
+def main():
+    last_check_time = str(datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ"))
+
+    while True:
+        with open("repo.json", "r") as file:
+            repositories = json.load(file)
+
+        threads = []
+        for repo in repositories:
+            th = threading.Thread(target=check_and_send, args=(repo, last_check_time))
+            threads.append(th)
+            th.start()
+        
+        last_check_time = str(datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ"))
+
+        for th in threads:
+            th.join()
+
+        time.sleep(300)
+
+
+if __name__ == "__main__":
+    main()
